c3b4f5aea183612c413b2f5af7a7a1b6438c9745
unknown <prattyancha26@gmail.com>
2023-12-31 22:36:30 +0530

    inital

--- backendnode/index.js
+++ backendnode/index.js
@@ -1,23 +1,9 @@
 const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose")
+
 const app = express();
-// ePHWdLFUMmKifyga   mongodb+srv://prattyancha009:<password>@clusternext.e7szttg.mongodb.net/
-app.listen(4000,() => {
-    console.log("server Started on PORT 4000");
-});
 
-mongoose.connect("mongodb://localhost:27017/jwt",{
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-}).then(() => {
-    console.log("DB Connection Successfull")
-})
+app.get('/', (req,res) => res.send("API RUNNING..."))
 
-app.use(cors({
-    origin:["http:/localhost:3000"],
-    method :["GET", "POST"],
-    credentials : true
-}))
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.listen(PORT, () => console.log(`server on port${PORT}`))
